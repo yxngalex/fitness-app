aa309c4ffa87b7238edba566a8f1938e35f9abc4
Aleksa Cekić <aleksa.cekic8@gmail.com>
2023-11-23 23:53:44 +0100

    addec cors config

--- src/main/java/org/alx/fitnessapp/model/dto/GoalDTO.java
+++ src/main/java/org/alx/fitnessapp/model/dto/GoalDTO.java
@@ -5,9 +5,7 @@ import lombok.Data;
 
 @Data
 public class GoalDTO {
-
     private Double weightGoal;
     private String bodyTypeGoal;
     private Integer weeklyExercise;
-
 }
---
+++ src/test/java/org/alx/fitnessapp/controller/CategoryControllerTest.java
@@ -0,0 +1,6 @@
+package org.alx.fitnessapp.controller;
+
+import org.alx.fitnessapp.config.ConfigBaseTest;
+
+public class CategoryControllerTest extends ConfigBaseTest {
+}
